08f6de150d773dd10f774e60a2438bd4cf228033
Akshaya Shaji <akshayashaji66@gmail.com>
2025-05-30 12:41:55 +0530

    action given to menu and navlist created

--- script.js
+++ script.js
@@ -10,10 +10,12 @@ headerCon.innerHTML=`
   <div class="header-logo-wrapper">
   </div>
   <button class="header-burger-menu">
-  </button>`
+  </button>
+  <nav id="header-nav"></nav>`
 
 const headerlogo=document.querySelector(".header-logo-wrapper")
 const burgerBtn=document.querySelector(".header-burger-menu")
+const nav=document.getElementById("header-nav")
 
 headerlogo.innerHTML=`
   <a href="index.html" class="logo-link"></a>
@@ -31,5 +33,53 @@ headerSlogan.innerHTML=`
    </p>`
 burgerBtn.innerHTML=`
     <svg class="pw-m-header__icon pw-m-header__icon-menu" style="display: block;"><use xlink:href="#icon-burger-menu"></use></svg>`
+    
+burgerBtn.addEventListener("click",()=>{
+    menuAdd();
+})    
+function menuAdd(){
+    logoLink.style.opacity="0"
+    headerSlogan.style.opacity="1"
+    burgerBtn.innerHTML=`<svg class="pw-m-header__icon pw-m-header__icon-close" style="display: block;"><use xlink:href="#icon-close"></use></svg>` 
+    burgerBtn.addEventListener("click",()=>{
+        menuClose()
+       })  
+}
+function menuClose(){
+    logoLink.style.opacity="1"
+    headerSlogan.style.opacity="0"
+    burgerBtn.innerHTML=`
+    <svg class="pw-m-header__icon pw-m-header__icon-menu" style="display: block;"><use xlink:href="#icon-burger-menu"></use></svg>`
+    burgerBtn.addEventListener("click",()=>{
+        menuAdd()
+       }) 
+}
 
+nav.innerHTML=`
+<div id="nav-con"></div>`
+
+const navCon=document.getElementById("nav-con")
+
+navCon.innerHTML=`
+<div id="nav-item"></div>
+<div id="search-con"></div>`
+
+const navItem=document.getElementById("nav-item")
+const searchCon=document.getElementById("search-con")
+
+navItem.innerHTML=`
+<ul id="nav-ul"></ul>`
+
+const navItemUl=document.getElementById("nav-ul")
+createNav()
+function createNav(){
+    let ItemName=["Home","Circular World","Digital Jounery","Electric Future","Freude","Models"]
+    let ItemLink=["index.html","index.html","index.html","index.html","index.html","index.html",]
+    for (let i=0;i<ItemName.length;i++){
+       let li=document.createElement("li")
+       li.innerHTML=`
+        <a href="${ItemLink[i]}">${ItemName[i]}</a>`
+        navItemUl.appendChild(li)
+    }
+}
 
